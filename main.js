e3c892bb2414f6bcb6b79ed4b7272d89cc842272
Bishan Webeshete <webesheteb@gmail.com>
2023-02-17 20:17:30 +0000

    updated table and javascript

--- main.js
+++ main.js
@@ -6,11 +6,35 @@ var data = {
 };
 
 var $modal = document.querySelector('.modal');
+var $entryButton = document.querySelector('.add-entry-button');
+var $form = document.querySelector('form');
 
-$modal.addEventListener('click', function () {
-  var $submitButton = document.querySelector('#submit');
-  if (event.target.matches($submitButton)) {
-    $modal.classList.add('hidden');
-  }
-})
-;
+$entryButton.addEventListener('click', function () {
+  $modal.classList.remove('hidden');
+});
+
+$form.addEventListener('submit', function () {
+  event.preventDefault();
+  var formEntry = {
+  };
+  formEntry.day = $form.elements['day-of-week'].value;
+  formEntry.time = $form.elements.time.value;
+  formEntry.description = $form.elements.description.value;
+  formEntry.entryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.push(formEntry);
+  $modal.classList.add('hidden');
+  $form.reset();
+});
+
+window.addEventListener('beforeunload', plannerLocalStorage);
+
+function plannerLocalStorage(event) {
+  var dataJson = JSON.stringify(data);
+  var dataLocalStorage = localStorage.setItem('planner-local-storage', dataJson);
+  return dataLocalStorage;
+}
+
+if (localStorage.getItem('planner-local-storage')) {
+  data = JSON.parse(localStorage.getItem('planner-local-storage'));
+}
