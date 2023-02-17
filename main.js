9bcbe43e5869801379a543dd81ffca59268d3df6
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-02-17 19:01:30 +0000

    started modal event listener

--- main.js
+++ main.js
@@ -4,3 +4,13 @@ var data = {
   editing: null,
   view: 'Monday'
 };
+
+var $modal = document.querySelector('.modal');
+
+$modal.addEventListener('click', function () {
+  var $submitButton = document.querySelector('#submit');
+  if (event.target.matches($submitButton)) {
+    $modal.classList.add('hidden');
+  }
+})
+;
